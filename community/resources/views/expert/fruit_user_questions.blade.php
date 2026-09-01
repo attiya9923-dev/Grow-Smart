dbc3ac0bffefa67307ef567b8f0da2af0a838edf
Abeeha604 <abeehaamanatali01@gmail.com>
2026-09-01 10:36:10 +0500

    update file

--- community/resources/views/expert/fruit_user_questions.blade.php
+++ community/resources/views/expert/fruit_user_questions.blade.php
@@ -486,7 +486,6 @@
                     name="answer_image" 
                     class="image-input" 
                     accept="image/*" 
-                    capture="environment" 
                 > 
 
             </div> 
